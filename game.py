9ead8dddc083b67d94aeb77ccaffadfc5e02b977
WangYifei <wangyifei@WangYifeis-MBP.lan>
2016-06-26 19:55:12 +0200

    updated input function and moved count

--- game.py
+++ game.py
@@ -1,39 +1,29 @@
 from random import randint
 def start():
 	print("Hello and welcome to this guessing game! \nStart guessing, it's a number between 1 and 1000...")
-	correct_number = randint(1,5) #correct_number=100
-	number=input() # number='200'
+	correct_number = randint(1,1000) 
+	number=input() 
 	count=1
 
-	while number != correct_number: # n=0,correct=100
+	while number != correct_number: 
+		
 		if number == 'quit':
 			print('**** The game was interrupted')
 			return None
 		elif type(number) is not int:
-			print('Stupid, only number is allowed')
-			number = input()
-			count = count + 1
+			number = input('Stupid, only number is allowed\n')
 		elif number > correct_number and number < 1000:
-			print('Guess is too high!')
-			number = input() #32
-			count = count + 1
-
+			number = input('Guess is too high!\n')
 		elif number < correct_number:
-			print('Guess is too low!')
-			number = input() #0
-			count = count + 1
+			number = input('Guess is too low!\n')
 		elif number < 1 or number > 1000:
-			print("Stupid guess! I won't count that...")
-			number = input() #100
-			count = count + 1
-
+			number = input("Stupid guess! I won't count that...\n") 
 		
-
-	print '**** CORRECT!\nYou guessed the correct number in', count, 'guesses'
-	print('Please enter your name:') 
-	input()
-	print('Do you want to play again?(y/n)')
-	answer = input()
+		count = count + 1
+   	
+   	print '**** CORRECT!\nYou guessed the correct number in', count, 'guesses'
+	input('Please enter your name:\n')
+	answer = input('Do you want to play again?(y/n)\n')
 	if answer == 'n':
 		print('**** The game is over')
 	elif answer == 'y':
